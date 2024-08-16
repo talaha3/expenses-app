be758e11a66f5dd1fe794fa96c0f43b44abcf18d
talaha3 <talahamodak33@gmail.com>
2024-08-16 07:54:39 +0100

    Create terminal frame sections

--- src/UI.rs
+++ src/UI.rs
@@ -1,22 +1,55 @@
-pub fn ui(frame: &mut Frame, app : &App){
-  todo();
+use ratatui::{
+    layout::{Constraint, Direction, Layout, Rect},
+    style::{Color, Style},
+    text::{Line, Span, Text},
+    widgets::{Block, Borders, Clear, List, ListItem, Paragraph, Wrap},
+    Frame,
+};
+
+use crate::app::{App, CurrentScreen, CurrentlyEditing};
+
+pub fn ui(frame: &mut Frame, app: &App) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Length(3),
+            Constraint::Min(1),
+            Constraint::Length(3),
+        ])
+        .split(frame.area());
+
+    let title_block = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default());
+
+    let title = Paragraph::new(Text::styled(
+        "Create New Json",
+        Style::default().fg(Color::Green),
+    ))
+    .block(title_block);
+
+    frame.render_widget(title, chunks[0]);
 }
 
 /// helper function to create a centered rect using up certain percentage of the available rect `r`
 fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
-  // Cut the given rectangle into three vertical pieces
-  let popup_layout = Layout::default().direction(Direction::Vertical).constraints([
-      Constraint::Percentage((100 - percent_y) / 2),
-      Constraint::Percentage(percent_y),
-      Constraint::Percentage((100-percent_y)/2)
-  ])
-  .split(r);
+    // Cut the given rectangle into three vertical pieces
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage((100 - percent_y) / 2),
+            Constraint::Percentage(percent_y),
+            Constraint::Percentage((100 - percent_y) / 2),
+        ])
+        .split(r);
 
-  // Then cut the middle vertical piece into three width-wise pieces
-  Layout::default().direction(Direction::Horizontal).constraints([
-      Constraint::Percentage((100-percent_x)/2),
-      Constraint::Percentage(percent_x),
-      Constraint::Percentage((100-percent_x)/2)
-  ])
-  .split(popup_layout[1])[1] // Return the middle chunk
+    // Then cut the middle vertical piece into three width-wise pieces
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage((100 - percent_x) / 2),
+            Constraint::Percentage(percent_x),
+            Constraint::Percentage((100 - percent_x) / 2),
+        ])
+        .split(popup_layout[1])[1] // Return the middle chunk
 }
